b7640d4a00deb1575e1a640803da25886d94bd7d
jaasonw <jayywong92@gmail.com>
2023-02-21 15:01:19 -0800

    load articles concurrently

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -23,68 +23,92 @@ export interface NewsArticle {
 export const load = (async () => {
   const payload: any = {};
   let parser = new Parser();
-  payload["fivethirtyeight"] = await parser.parseURL(
-    "https://fivethirtyeight.com/all/feed"
-  );
-  payload["New York Times"] = await parser.parseURL(
-    "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-  );
-  payload["Los Angeles Times"] = await parser.parseURL(
-    "https://www.latimes.com/world-nation/rss2.0.xml#nt=0000016c-0bf3-d57d-afed-2fff84fd0000-1col-7030col1"
-  );
-
-  payload["ABC"] = await parser.parseURL("https://abc7.com/feed/");
-
   // preprocess rss feeds, everything has a different format for what
   const processed: NewsArticle[] = [];
-  for (let article of payload["fivethirtyeight"].items) {
-    processed.push({
-      title: article.title as string,
-      author: article.creator as string,
-      link: article.link as string,
-      description: htmlDecode(article.content as string) ?? "",
-      date: dayjs(article.isoDate as string).toString(),
-      source: "FiveThirtyEight",
-      sentiment: null,
-      topics: article.categories,
-    });
-  }
-  for (let article of payload["New York Times"].items) {
-    processed.push({
-      title: article.title as string,
-      author: (article.creator as string) ?? "N/A",
-      link: article.link as string,
-      description: article.content as string,
-      date: dayjs(article.isoDate as string).toString(),
-      source: "New York Times",
-      sentiment: null,
-      topics: article?.categories?.map((e: any) => e["_"]),
-    });
-  }
-  for (let article of payload["Los Angeles Times"].items) {
-    processed.push({
-      title: article.title as string,
-      author: (article.creator as string) ?? "N/A",
-      link: article.link as string,
-      description: article.contentSnippet as string,
-      date: dayjs(article.isoDate as string).toString(),
-      source: "Los Angeles Times",
-      sentiment: null,
-    });
-  }
+  await Promise.all([
+    parser.parseURL("https://fivethirtyeight.com/all/feed").then((payload) => {
+      for (let article of payload.items) {
+        processed.push({
+          title: article.title as string,
+          author: article.creator as string,
+          link: article.link as string,
+          description: htmlDecode(article.content as string) ?? "",
+          date: dayjs(article.isoDate as string).toString(),
+          source: "FiveThirtyEight",
+          sentiment: null,
+          topics: article.categories,
+        });
+      }
+    }),
+    parser
+      .parseURL("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
+      .then((payload) => {
+        for (let article of payload.items) {
+          processed.push({
+            title: article.title as string,
+            author: (article.creator as string) ?? "N/A",
+            link: article.link as string,
+            description: article.content as string,
+            date: dayjs(article.isoDate as string).toString(),
+            source: "New York Times",
+            sentiment: null,
+            topics: article?.categories?.map((e: any) => e["_"]),
+          });
+        }
+      }),
+    parser
+      .parseURL(
+        "https://www.latimes.com/world-nation/rss2.0.xml#nt=0000016c-0bf3-d57d-afed-2fff84fd0000-1col-7030col1"
+      )
+      .then((payload) => {
+        for (let article of payload.items) {
+          processed.push({
+            title: article.title as string,
+            author: (article.creator as string) ?? "N/A",
+            link: article.link as string,
+            description: article.contentSnippet as string,
+            date: dayjs(article.isoDate as string).toString(),
+            source: "Los Angeles Times",
+            sentiment: null,
+          });
+        }
+      }),
 
-  for (let article of payload["ABC"].items) {
-    processed.push({
-      title: article.title as string,
-      author: (article.creator as string) ?? "N/A",
-      link: article.link as string,
-      description: article.contentSnippet as string,
-      date: dayjs(article.isoDate as string).toString(),
-      source: "ABC",
-      sentiment: null,
-      topics: article?.categories[0]?.replace(/^\s+|\s+$/g, "").split(","),
-    });
-  }
+    parser.parseURL("https://abc7.com/feed/").then((payload) => {
+      for (let article of payload.items) {
+        if (!article || !article.categories) return;
+        processed.push({
+          title: article.title as string,
+          author: (article.creator as string) ?? "N/A",
+          link: article.link as string,
+          description: article.contentSnippet as string,
+          date: dayjs(article.isoDate as string).toString(),
+          source: "ABC",
+          sentiment: null,
+          topics: article?.categories[0]?.replace(/^\s+|\s+$/g, "").split(","),
+        });
+      }
+    }),
+
+    parser
+      .parseURL(
+        "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114"
+      )
+      .then((payload) => {
+        for (let article of payload.items) {
+          if (!article || !article.categories) return;
+          processed.push({
+            title: article.title as string,
+            author: (article.creator as string) ?? "N/A",
+            link: article.link as string,
+            description: article.contentSnippet as string,
+            date: dayjs(article.isoDate as string).toString(),
+            source: "CNBC",
+            sentiment: null,
+          });
+        }
+      }),
+  ]);
 
   return { articles: JSON.stringify(processed) };
 }) satisfies PageServerLoad;
