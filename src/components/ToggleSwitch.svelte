3a861fa5178420b554d1e6ce9cc18a40a35e5a2e
jason wong <jayywong92@gmail.com>
2023-02-23 20:04:33 -0800

    refactor switches into separate components (#3)
    * refactor switches into separate components
    
    * test commit to see if the autoformat check works
    
    * fix format check
    
    * that did not work
    
    * cool it worked
    
    * revert comment
    
    * properly indent comment

---
+++ src/components/ToggleSwitch.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let checked: boolean;
+</script>
+
+<label class="relative inline-flex items-center cursor-pointer">
+  <input type="checkbox" value="" class="sr-only peer" bind:checked />
+  <div
+    class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"
+  />
+  <span class="ml-3 text-sm font-medium"><slot /></span>
+</label>
