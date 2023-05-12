35d7adc2840e81b3aa95b07513d69ba685b01696
Ronald Lee <ronaldvlee9@gmail.com>
2023-05-11 18:46:31 -0700

    changed a number

--- src/components/ArticleList.svelte
+++ src/components/ArticleList.svelte
@@ -16,14 +16,14 @@
   export let neutralNews: boolean;
   export let negativeNews: boolean;
 
-  const itemsPerPage = 5;
+  const itemsPerPage = 6;
   const maxPages = Math.floor(posts.length / itemsPerPage);
 </script>
 
 <div id="articlelist" class="">
   {#each posts.slice(
       itemsPerPage * Number($page.url.searchParams.get('page')), 
-      itemsPerPage * Number($page.url.searchParams.get('page')) + itemsPerPage
+      (itemsPerPage * Number($page.url.searchParams.get('page')) + itemsPerPage) < posts.length ? (itemsPerPage * Number($page.url.searchParams.get('page')) + itemsPerPage) : posts.length -1 
     ).filter((e) => {
     if (!positiveNews && (e.sentiment ?? 0) > 10) return false;
     if (!negativeNews && (e.sentiment ?? 0) < -10) return false;
@@ -42,11 +42,11 @@
   {/each}
 </div>
 <div class = "my-5">
-  {#if Number($page.url.searchParams.get('page')) != 0}
+  {#if Number($page.url.searchParams.get('page')) > 0}
     <a class="float-left block lg:inline-block text-sm px-5 py-3 leading-none mt-4 rounded lg:mt-0 lg:bg-blue-600 lg:text-white lg:font-bold" href="?page={Number($page.url.searchParams.get('page'))-1}">Previous</a>
   {/if}
 
-  {#if Number($page.url.searchParams.get('page')) != maxPages}
+  {#if Number($page.url.searchParams.get('page')) < maxPages}
     <a class="float-right block lg:inline-block text-sm px-5 py-3 leading-none mt-4 rounded lg:mt-0 lg:bg-blue-600 lg:text-white lg:font-bold" href="?page={Number($page.url.searchParams.get('page'))+1}">Next</a>
   {/if}
 </div>
